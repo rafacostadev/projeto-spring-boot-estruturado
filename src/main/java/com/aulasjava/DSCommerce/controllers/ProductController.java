10b8503d298e681fe3e82bfa25dea8cec56e1c05
rafacostadev <rafaelcostamantis@gmail.com>
2024-12-08 13:24:13 -0300

    Adicionados endpoints dos produtos

--- src/main/java/com/aulasjava/DSCommerce/controllers/ProductController.java
+++ src/main/java/com/aulasjava/DSCommerce/controllers/ProductController.java
@@ -1,12 +1,20 @@
 package com.aulasjava.DSCommerce.controllers;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.aulasjava.DSCommerce.DTO.ProductDTO;
 import com.aulasjava.DSCommerce.services.ProductService;
@@ -16,9 +24,34 @@ import com.aulasjava.DSCommerce.services.ProductService;
 public class ProductController {
 	@Autowired
 	private ProductService service;
-	
+
 	@GetMapping
 	public ResponseEntity<Page<ProductDTO>> findAll(Pageable pageable) {
 		return ResponseEntity.ok(service.findAll(pageable));
 	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
+		return ResponseEntity.ok(service.findById(id));
+	}
+
+	@PostMapping
+	public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto) {
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<ProductDTO> insert(@PathVariable Long id, @RequestBody ProductDTO dto) {
+		dto = service.update(id, dto);
+		return ResponseEntity.ok((dto));
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+
 }
--- src/main/java/com/aulasjava/DSCommerce/services/ProductService.java
+++ src/main/java/com/aulasjava/DSCommerce/services/ProductService.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.aulasjava.DSCommerce.DTO.ProductDTO;
+import com.aulasjava.DSCommerce.entities.Product;
 import com.aulasjava.DSCommerce.repositories.ProductRepository;
 
 @Service
@@ -13,7 +15,39 @@ public class ProductService {
 	@Autowired
 	private ProductRepository repository;
 
+	@Transactional(readOnly = true)
 	public Page<ProductDTO> findAll(Pageable pageable) {
 		return repository.findAll(pageable).map(x -> new ProductDTO(x));
+	}
+
+	@Transactional(readOnly = true)
+	public ProductDTO findById(Long id) {
+		return new ProductDTO(repository.findById(id).get());
+	}
+
+	@Transactional
+	public ProductDTO insert(ProductDTO dto) {
+		Product product = new Product();
+		DtoToProduct(dto, product);
+		return new ProductDTO(repository.save(product));
+	}
+	@Transactional
+	public ProductDTO update(Long id, ProductDTO dto) {
+		Product product = repository.getReferenceById(id);
+		DtoToProduct(dto, product);
+		return new ProductDTO(repository.save(product));
+	}
+	
+	@Transactional
+	public void delete(Long id) {
+		repository.deleteById(id);
+	}
+	
+	
+	private void DtoToProduct(ProductDTO dto, Product product) {
+		product.setDescription(dto.getDescription());
+		product.setImgUrl(dto.getImgUrl());
+		product.setName(dto.getName());
+		product.setPrice(dto.getPrice());
 	}
 }
